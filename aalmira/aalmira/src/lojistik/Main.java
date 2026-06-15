2ccaad695f37f0f0e36e374d96f0bd9d35a355b1
Almira <almirakahramann@gmail.com>
2026-06-15 13:50:51 +0300

    Update Main.java

--- aalmira/aalmira/src/lojistik/Main.java
+++ aalmira/aalmira/src/lojistik/Main.java
@@ -29,24 +29,24 @@ public class Main {
         Konum depo = new Konum(0, 0);
 
         // Ornek musteriler
-        Musteri ayse = new Musteri(1, "Ayse", new Konum(3, 4), false);      // normal sokak
-        Musteri mehmet = new Musteri(2, "Mehmet", new Konum(6, 8), true);   // DAR sokak
-        Musteri zeynep = new Musteri(3, "Zeynep", new Konum(1, 1), false);  // normal sokak
+        Musteri almira = new Musteri(1, "Almira", new Konum(3, 4), false);      // normal sokak
+        Musteri emriye = new Musteri(2, "Emriye", new Konum(6, 8), true);       // DAR sokak
+        Musteri hilal = new Musteri(3, "Hilal", new Konum(1, 1), false);        // normal sokak
 
         baslik("SENARYO 1: Normal kamyon teslimati");
         Kamyon kamyon = new Kamyon("34-ABC-01", 1000, 50, 90, depo, logger);
         Paket paket1 = new Paket(20, "Buzdolabi");
-        kamyon.teslimatYap(ayse, paket1);
+        kamyon.teslimatYap(almira, paket1);
 
         baslik("SENARYO 2: Kamyonun dar sokaga teslimat yapamamasi");
         Paket paket2 = new Paket(15, "Camasir Makinesi");
-        kamyon.teslimatYap(mehmet, paket2); // Mehmet dar sokakta -> basarisiz
+        kamyon.teslimatYap(emriye, paket2); // Emriye dar sokakta -> basarisiz
 
         baslik("SENARYO 3: Drone'un hafif paketi teslim etmesi");
         Drone drone = new Drone("DRN-007", 5, 30, 120, depo, logger);
         Paket paket3 = new Paket(2, "Ilac Kutusu");
         try {
-            drone.teslimatYap(zeynep, paket3); // 2 kg < 5 kg -> basarili
+            drone.teslimatYap(hilal, paket3); // 2 kg < 5 kg -> basarili
         } catch (AgirPaketException e) {
             logger.hataYaz(e.getMessage());
         }
@@ -54,7 +54,7 @@ public class Main {
         baslik("SENARYO 4: Drone'un agir paket icin AgirPaketException firlatmasi");
         Paket paket4 = new Paket(7, "Kitap Kolisi");
         try {
-            drone.teslimatYap(ayse, paket4); // 7 kg >= 5 kg -> istisna
+            drone.teslimatYap(almira, paket4); // 7 kg >= 5 kg -> istisna
         } catch (AgirPaketException e) {
             // HATA YONETIMI: ozel istisna burada yakalanir.
             logger.hataYaz("Yakalanan istisna -> " + e.getMessage());
@@ -63,27 +63,27 @@ public class Main {
         baslik("SENARYO 5: Motorun 10 kg uzeri pakette hizinin %20 azalmasi");
         Motor motor = new Motor("34-MTR-09", 50, 40, 100, depo, logger);
         Paket paket5 = new Paket(12, "Spor Malzemesi"); // 12 kg > 10 kg
-        motor.teslimatYap(zeynep, paket5);
+        motor.teslimatYap(hilal, paket5);
 
         baslik("SENARYO 6: Yakit yetersizse yakit ikmali yapilmasi");
         // Yakiti cok dusuk bir motor olusturuyoruz, uzak musteriye teslimat deneyecek.
         Motor azYakitliMotor = new Motor("34-MTR-00", 50, 2, 100, depo, logger);
         Paket paket6 = new Paket(3, "Telefon");
-        azYakitliMotor.teslimatYap(ayse, paket6); // mesafe=5, yakit=2 -> ikmal gerekir
+        azYakitliMotor.teslimatYap(almira, paket6); // mesafe=5, yakit=2 -> ikmal gerekir
 
         baslik("SENARYO 7: Paketlerin FIFO sirasiyla kuyruktan cekilmesi");
         LojistikSistemi sistem = new LojistikSistemi(logger);
-        sistem.musteriEkle(ayse);
-        sistem.musteriEkle(zeynep);
+        sistem.musteriEkle(almira);
+        sistem.musteriEkle(hilal);
 
         // Teslimati yapacak araclar (kapasitesi yuksek kamyon ilk sirada)
         Kamyon dagitimKamyonu = new Kamyon("34-XYZ-99", 1000, 100, 90, depo, logger);
         sistem.aracEkle(dagitimKamyonu);
 
         // Paketler kuyruga giris SIRASIYLA eklenir (1 -> 2 -> 3)
-        sistem.paketiKuyrugaEkle(new Paket(4, "Paket-A", ayse));
-        sistem.paketiKuyrugaEkle(new Paket(6, "Paket-B", zeynep));
-        sistem.paketiKuyrugaEkle(new Paket(8, "Paket-C", ayse));
+        sistem.paketiKuyrugaEkle(new Paket(4, "Paket-A", almira));
+        sistem.paketiKuyrugaEkle(new Paket(6, "Paket-B", hilal));
+        sistem.paketiKuyrugaEkle(new Paket(8, "Paket-C", almira));
 
         // FIFO: ilk eklenen (Paket-A) ilk teslim edilir.
         sistem.teslimatlariBaslat();
